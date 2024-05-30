1d99d25afdfe668a5c5a5e409f85fe742d5741df
asimdaud <asim.scorpio9@gmail.com>
2024-05-30 19:20:56 +0100

    added jenkins ec2 setup

---
+++ apps/jenkins/terraform/efs.tf
@@ -0,0 +1,5 @@
+module "efs_module" {
+  source = "./modules/efs"
+  vpc_id     = "vpc-0a5ca4a92c2e10163"
+  subnet_ids = ["subnet-058a7514ba8adbb07", "subnet-0dbcd1ac168414927", "subnet-032f5077729435858"]
+}
---
+++ apps/jenkins/terraform/iam.tf
@@ -0,0 +1,6 @@
+module "jenkins_iam" {
+  source = "./modules/iam"
+  instance_profile_name = "jenkins-instance-profile"
+  iam_policy_name       = "jenkins-iam-policy"
+  role_name             = "jenkins-role"
+}
