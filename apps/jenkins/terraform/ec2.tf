0618ecd4994cac86a6c90e5a5c070224f050338d
asimdaud <asim.scorpio9@gmail.com>
2024-05-31 16:01:20 +0100

    removing tf-lint

--- apps/jenkins/terraform/ec2.tf
+++ apps/jenkins/terraform/ec2.tf
@@ -3,7 +3,7 @@ module "ec2_instance" {
 
   instance_name      = "jenkins-agent"
   ami_id             = "ami-0e68ab34763bcba1f"
-  instance_type      = "t1.2x.small"
+  instance_type      = "t1.small"
   key_name           = "asimdaud"
   subnet_ids         = ["subnet-058a7514ba8adbb07", "subnet-0dbcd1ac168414927", "subnet-032f5077729435858"]
   instance_count     = 1
